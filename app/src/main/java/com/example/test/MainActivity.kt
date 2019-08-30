ef1efe27598a4a0ac64377c69d9d5502d95f2eba
Pascal Bertschi <pascal@bertschi.io>
2019-08-30 18:49:00 +0200

    init

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.test
+
+import android.app.Activity
+import android.os.Bundle
+import android.provider.Settings
+import android.webkit.WebView
+import android.provider.Settings.Secure
+
+
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.loadUrl("https://order.mlinkdev.ortho-team.ch")
+
+        val androidId = Settings.Secure.getString(
+            contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+
+
+    }
+}
